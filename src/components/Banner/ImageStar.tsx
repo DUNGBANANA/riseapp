a489497248eaca45f7fb34a4c99e065da55302af
DUNGNC <congdung25122001@gmail.com>
2022-10-19 16:45:42 +0700

    update

--- src/components/Banner/ImageStar.tsx
+++ src/components/Banner/ImageStar.tsx
@@ -1,5 +1,6 @@
 export default function ImageRateStar() {
     return (
+        // eslint-disable-next-line jsx-a11y/img-redundant-alt
         <img loading="lazy"
             src="https://riseapps.co/wp-content/webp-express/webp-images/doc-root/wp-content/themes/riseapps/dist/images/star.png.webp"
             alt="star image" />
